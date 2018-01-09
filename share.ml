80317d51795d8685614f4101492406ea701c9e30
Reynir Björnsson <rbj@isnordic.dk>
2018-01-09 10:13:23 +0100

    Code dump \o/

--- share.ml
+++ share.ml
@@ -1,3 +1,14 @@
+let sum a =
+  Array.fold_left
+    GF256.add
+    GF256.zero
+    a
+
+let product a =
+  Array.fold_left
+    GF256.mul
+    GF256.one
+    a
 
 let f (a : GF256.t array) (x : GF256.t) =
   let rec loop i a x acc =
@@ -19,4 +30,26 @@ let share_byte secret threshold shares =
          then secret
          else Cstruct.get_uint8 a (i-1)) in
   Array.init shares succ
-  |> Array.map (f a)
+  |> Array.map (fun x -> x, f a x)
+
+let l u i =
+  Array.mapi 
+    (fun j u_j ->
+       let open GF256 in
+       if i = j
+       then one
+       else div u_j (add u_j u.(i)))
+    u
+  |> product
+
+let i u v =
+  Array.mapi
+    (fun i v_i ->
+       GF256.mul (l u i) v_i)
+    v
+  |> sum
+
+let unshare_byte shares =
+  let u = Array.map fst shares
+  and v = Array.map snd shares in
+  i u v |> GF256.to_char
